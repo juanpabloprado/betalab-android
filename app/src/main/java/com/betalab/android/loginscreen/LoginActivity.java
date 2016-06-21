49f20bd0f247d0d632e1d40c92899a41c9fcddd5
Juan Pablo Prado <juan@juanpabloprado.com>
2016-06-21 02:45:47 -0500

    Adding layouts for login

---
+++ app/src/main/java/com/betalab/android/loginscreen/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.betalab.android.loginscreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import com.betalab.android.base.activity.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
