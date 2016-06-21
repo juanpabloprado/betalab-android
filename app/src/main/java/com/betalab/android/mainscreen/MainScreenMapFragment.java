6e4d29fc82fb5466b512167c6b20c60b15c2c8c5
Sergio Adrián Lagunas Pinacho <Shekomaru@Sergios-MacBook-Pro.local>
2016-06-21 08:40:43 -0500

    Avances hechos en el mapa En develop, sí, pero cambios estables

--- app/src/main/java/com/betalab/android/mainscreen/MainScreenMapFragment.java
+++ app/src/main/java/com/betalab/android/mainscreen/MainScreenMapFragment.java
@@ -1,7 +1,11 @@
 package com.betalab.android.mainscreen;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,7 +30,8 @@ import java.util.Random;
 /**
  * Created by Shekomaru on 6/20/16.
  */
-public class MainScreenMapFragment extends Fragment implements OnMapReadyCallback {
+public class MainScreenMapFragment extends Fragment
+    implements OnMapReadyCallback, View.OnClickListener {
   private static final String TAG = "MainScreenMapFragment";
 
   private GoogleMap mMap;
@@ -47,18 +52,77 @@ public class MainScreenMapFragment extends Fragment implements OnMapReadyCallbac
     SupportMapFragment mapFragment =
         (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
     mapFragment.getMapAsync(this);
+
+    view.findViewById(R.id.fab_mainscreen_map_add).setOnClickListener(this);
   }
 
   @Override public void onMapReady(GoogleMap googleMap) {
     mMap = googleMap;
 
+    if (!(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(getContext(),
+        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+      // TODO: Consider calling
+      //    ActivityCompat#requestPermissions
+      // here to request the missing permissions, and then overriding
+      //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+      //                                          int[] grantResults)
+      // to handle the case where the user grants the permission. See the documentation
+      // for ActivityCompat#requestPermissions for more details.
+      mMap.setMyLocationEnabled(true);
+    } else {
+      String[] permiss = new String[2];
+      permiss[0] = Manifest.permission.ACCESS_FINE_LOCATION;
+      permiss[1] = Manifest.permission.ACCESS_COARSE_LOCATION;
+      requestPermissions(permiss, 1234);
+    }
     // Add a marker in Sydney and move the camera
 
     loadGooglePins(googleMap);
 
+    googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+      @Override public boolean onMarkerClick(Marker marker) {
+        if (incidenceMap != null && incidenceMap.containsKey(marker)) {
+          Incidence incidence = incidenceMap.get(marker);
+          // TODO: 6/21/16 Show things and stuff
+        }
+        return false;
+      }
+    });
+
     /*LatLng sydney = new LatLng(-34, 151);
     mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
     mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+  }
+
+  @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+      @NonNull int[] grantResults) {
+    Log.d(TAG, String.format(
+        "onRequestPermissionsResult() called with: requestCode = [%d], permissions = [%s], grantResults = [%s]",
+        requestCode, permissions, grantResults));
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    switch (requestCode) {
+      case 1234:
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED
+            || grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+          if (ActivityCompat.checkSelfPermission(getContext(),
+              Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+              && ActivityCompat.checkSelfPermission(getContext(),
+              Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+          }
+          mMap.setMyLocationEnabled(true);
+        }
+    }
   }
 
   private void loadGooglePins(GoogleMap googleMap) {
@@ -109,5 +173,14 @@ public class MainScreenMapFragment extends Fragment implements OnMapReadyCallbac
       incidences.add(incidence);
     }
     return incidences;
+  }
+
+  @Override public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.fab_mainscreen_map_add:
+        Log.d(TAG, "onClick() called with: " + "v = [" + String.valueOf(v.getId()) + "]");
+
+        break;
+    }
   }
 }
